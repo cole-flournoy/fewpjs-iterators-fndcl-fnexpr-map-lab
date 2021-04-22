0b86c1349a0615f6585a900b172e72f44883b95d
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-22 14:32:16 -0400

    commit tree dbe03a0b29985fd64df8ef64d59712c6ba03af51

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(title => {
+    const words = title.split(" ")
+    const newWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    return newWords.join(" ") 
+  })
 }
+
+titleCased()
